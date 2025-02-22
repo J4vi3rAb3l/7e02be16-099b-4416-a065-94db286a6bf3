e721a2324d74667e0f72b36d157345a1c9ec8957
J4vi3rAb3l <javierabelmezacresencio@gmail.com>
2025-02-22 15:19:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function actualizarFechaHora() {
     let fecha = new Date();
-    let opciones = { timeZone: 'America/Los_Angeles', hour12: true };
+    let opciones = { 
+        timeZone: 'America/Tijuana', 
+        day: '2-digit', month: '2-digit', year: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true 
+    };
     let fechaHora = fecha.toLocaleString('es-ES', opciones).replace(',', ' a las');
-    let fechaTexto = 'Mexicali B.C a: ' + fechaHora;
-    
-    let fechaElemento = document.getElementById('fecha-hora');
-    fechaElemento.innerText = fechaTexto;
-    fechaElemento.style.textAlign = 'center';
+    document.getElementById('fecha-hora').innerText = `Mexicali B.C a: ${fechaHora}`;
 }
-
-actualizarFechaHora();
 setInterval(actualizarFechaHora, 1000);
+actualizarFechaHora();
